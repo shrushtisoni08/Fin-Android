87e6ba4288821bd705612e51de931bae5c7ecb97
Shrushti Soni <shrushtisoni08@gmail.com>
2021-08-15 09:19:46 +0530

    Initialize repository

---
+++ app/src/main/java/com/example/finanalyze/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.finanalyze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        CountDownTimer countDownTimer = new CountDownTimer(3000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                startActivity(intent);
+            }
+        }.start();
+    }
+}
---
+++ app/src/main/java/com/example/finanalyze/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.finanalyze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity2 extends AppCompatActivity {
+    public void signUp(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(),SignUp.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
